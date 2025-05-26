b9ac9bade402f9fa2e9e8c1cfed91823c926e733
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-26 11:39:22 +0200

    Get, post, put y patch testeados

--- src/main/java/com/iron/person_service/models/Person.java
+++ src/main/java/com/iron/person_service/models/Person.java
@@ -6,33 +6,27 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "persons")
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
     @Id
-    @NonNull
     @NotBlank(message = "el nickname no puede ser nulo")
     private String nickName;
 
     @NotBlank(message = "el nombre completo no puede ser nulo")
-    @NonNull
     private String name;
 
 
     @Digits(integer = 9, fraction = 0, message = "El número de teléfono debe tener 9 dígitos")
-    @NonNull
     private int phoneNumber;
 
-    @NonNull
+
     @Pattern(regexp = "^[a-zA-Z0-9]+@gmail\\.com$", message = "El correo electrónico solo puede contener letras y números antes de '@gmail.com'")
     private String email;
 
--- src/test/java/com/iron/person_service/services/PersonServiceTest.java
+++ src/test/java/com/iron/person_service/services/PersonServiceTest.java
@@ -1,12 +1,25 @@
 package com.iron.person_service.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iron.person_service.dtos.UpdateEmailPersonDTO;
+import com.iron.person_service.dtos.UpdatePhoneNumberPersonDTO;
 import com.iron.person_service.models.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 
 @SpringBootTest
 class PersonServiceTest {
@@ -14,21 +27,107 @@ class PersonServiceTest {
     @Autowired
     PersonService personService;
 
-    @Test
-    @DisplayName("Guardo 2 personas")
-    void savePerson(){
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
-        Person person1, person2;
+    MockMvc mockMvc;
 
-        person1 = new Person("hombre_de_la_rae", "Víctor Sanz", 634666424, "victoralexsanzcarrasc@gmail.com");
+    final ObjectMapper objectMapper = new ObjectMapper();
 
-        person2 = new Person("auronplay", "Raúl Alvarez", 647951289, "raulalvarez@gmail.com");
-
-
-        personService.savePerson(person1);
-
-        personService.savePerson(person2);
+    @BeforeEach
+    void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
+    //POST
+
+    @Test
+    @DisplayName("Guardar una persona")
+    @Transactional //me aseguro que no se realice la modificación en la base de datos
+    void savePerson() throws Exception {
+        Person person = new Person("kino", "Joaquin Perez", 632459871, "kino@gmail.com");
+
+        String personJSON = objectMapper.writeValueAsString(person);
+
+        MvcResult result = mockMvc.perform(post("/api/person")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(personJSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    //GET
+
+    @Test
+    @DisplayName("Obtener todas las personas")
+    void findAllPersons() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/person/allPersons")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @DisplayName("Busco una persona por nickname que exista")
+    void findExistingPerson() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/person/hombre_de_la_rae")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @DisplayName("Busco una persona que no exista")
+    void findNonExistingPerson() throws Exception {
+
+        mockMvc.perform(get("/api/person/flow")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Esa persona no existe en la base de datos"));
+    }
+
+    //PATCH
+
+    @Test
+    @DisplayName("Actualizar email de una persona")
+    @Transactional
+    void updateEmail() throws Exception {
+
+        String newEmail = "gurrucharri@gmail.com";
+
+        UpdateEmailPersonDTO request = new UpdateEmailPersonDTO(newEmail);
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(patch("/api/person/updateEmail/developer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(newEmail));
+    }
+
+    @Test
+    @DisplayName("Actualizar número de teléfono de una persona")
+    @Transactional
+    void updatePhoneNumber() throws Exception {
+
+        int newPhoneNumber = 987654321;
+
+        UpdatePhoneNumberPersonDTO request = new UpdatePhoneNumberPersonDTO(newPhoneNumber);
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(patch("/api/person/updatePhoneNumber/developer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.phoneNumber").value(newPhoneNumber));
+    }
+
+
 
 
 
